c9de5a19e02820002b98298487bb319880d2fe57
kmlhpk <44464908+kmlhpk@users.noreply.github.com>
2023-08-11 16:30:45 +0100

    amended snakefile to capture generically-named input; created symlink utility script to supply said generic input

---
+++ muex/scripts/symlink_utility.py
@@ -0,0 +1,34 @@
+import defopt
+from pathlib import Path
+
+# TODO allow relative paths?
+# TODO print mappings to file
+
+def main(indir: str, outdir: str):
+    """
+    Creates consistently-named symlinks in `outdir` to all files in `indir`. Files are named `file0.fq.gz`,`file1.fq.gz`... in alphabetical order of their names in `indir`.
+
+    :param indir: Absolute path to directory containing input files
+    :param outdir: Absolute path to directory to contain desired symlinks
+    """
+    inpath = Path(indir)
+    if "~" in indir:
+        inpath = inpath.expanduser()
+    
+    outpath = Path(outdir)
+    if "~" in indir:
+        outpath = outpath.expanduser()
+
+    outpath.mkdir(parents=True, exist_ok=True)
+
+    files = sorted([x for x in list(inpath.iterdir()) if x.is_file()])
+    num_files = len(files)
+
+    for i in range(num_files):
+        symlink_filepath = outpath / f"file{i}.fq.gz"
+        symlink_filepath.symlink_to(files[i])
+
+    return
+
+if __name__ == "__main__":
+    defopt.run(main)
